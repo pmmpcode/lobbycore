dfc7a799ac2a188a63c2b654bc32993930fca791
pmmpcode <30850719+pmmpcode@users.noreply.github.com>
2017-08-08 18:59:40 -0700

    Update main.php

--- src/lobbycore/main.php
+++ src/lobbycore/main.php
@@ -1,1 +1,11 @@
-
+<?php
+namespace lobbycore;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+class main extends PluginBase {
+}
+?>
