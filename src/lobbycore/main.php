81111d615b2484892735eeabbcd5a1f1402a0fa3
pmmpcode <30850719+pmmpcode@users.noreply.github.com>
2017-08-08 19:08:43 -0700

    Started some of the lobby command, it is NOT ready for use.

--- src/lobbycore/main.php
+++ src/lobbycore/main.php
@@ -7,5 +7,21 @@ use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 class main extends PluginBase {
+    public function onEnable(){
+        if(!is_dir($this->getDataFolder()){
+            mkdir($this->getDataFolder());
+            $config = new Config($this->getDataFolder()."config.yml", CONFIG::YAML);
+            $this->getLogger()->info(TextFormat::YELLOW."Created config in ".$this->getDataFolder()."config.yml");
+        }
+        $this->getLogger()->info(TextFormat::GREEN."Active.");
+    }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if($command->getName() == "lobby"){
+            $config = new Config($this->getDataFolder()."config.yml", CONFIG::YAML);
+            if($config->exists("lobby-x") == false || $config->get("lobby-x") == ""){
+                $sender->sendMessage("We could not transfer you to the lobby because either the 'lobby-x' doesn't exist in the config or it's equal to nothing.");
+            }
+        }
+    }
 }
 ?>
